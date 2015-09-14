47487bf6efa4db07a2670399d9ba87a991af7ccd
Dennis Kurlinski <thepcg@gmail.com>
2015-09-13 21:58:33 -0400

    Version 0.1.0 - first release

--- server.js
+++ server.js
@@ -10,19 +10,11 @@ Author: Dennis J Kurlinski
 var getweather = require("./getweather");
 
 // Global Variables //
-var location = "12776596";
+var location = "12776596";//woeid for Madison, OH
+var outputPath = "C:\\Users\\Username\\Some Folder\\"
 
-getweather.getYahooWeather(location, __dirname + "/templates/conditions1.txt", __dirname + "/output_conditions.txt", function(err, data) {
-//getweather.getHTTPSWeather("api.forecast.io", "/forecast/3d2049b6bda9719bf81ad723e9b7c643/41.808,-81.080", __dirname + "/templates/template1.txt", __dirname + "/output.txt", function(err, data) {
-	console.log(data);
-});
+getweather.getYahooWeather(location, __dirname + "/templates/conditions1.txt", outputPath + "output_conditions.txt", function(err, data) {});
 
-getweather.getYahooWeather(location, __dirname + "/templates/today1.txt", __dirname + "/output_today.txt", function(err, data) {
-//getweather.getHTTPSWeather("api.forecast.io", "/forecast/3d2049b6bda9719bf81ad723e9b7c643/41.808,-81.080", __dirname + "/templates/template1.txt", __dirname + "/output.txt", function(err, data) {
-	console.log(data);
-});
+getweather.getYahooWeather(location, __dirname + "/templates/today1.txt", outputPath + "output_today.txt", function(err, data) {});
 
-getweather.getYahooWeather(location, __dirname + "/templates/tonight1.txt", __dirname + "/output_tonight.txt", function(err, data) {
-//getweather.getHTTPSWeather("api.forecast.io", "/forecast/3d2049b6bda9719bf81ad723e9b7c643/41.808,-81.080", __dirname + "/templates/template1.txt", __dirname + "/output.txt", function(err, data) {
-	console.log(data);
-});
+getweather.getYahooWeather(location, __dirname + "/templates/tonight1.txt", outputPath + "output_tonight.txt", function(err, data) {});
