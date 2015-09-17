ef9b0c815b1d884805de015fdf6e96c2d9688c59
Dennis Kurlinski <thepcg@gmail.com>
2015-09-17 19:12:05 -0400

    github#9 - incorrect calulation of time prior to noon addressed by fixing  issue with equal sign in the addLocalVariable function.

--- getweather.js
+++ getweather.js
@@ -154,7 +154,7 @@ var addLocalVariable = function() {
 		ampm = "PM";
 		if (hours > 12){ hours = hours - 12}
 	} else {
-		if (hours = 0){hours = 12}
+		if (hours == 0){hours = 12}
 		ampm = "AM";
 	}
 
